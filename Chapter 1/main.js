f9e4093fff44c5948cf9cb41b280e4adec18971b
n00nietzsche <n00nietzsche@gmail.com>
2018-11-03 11:08:23 +0900

    Chapter 1 Complete

---
+++ Chapter 1/main.js
@@ -0,0 +1,24 @@
+console.log('main.js loaded');
+'use strict';
+
+paper.install(window);
+paper.setup(document.getElementById('mainCanvas'));
+
+// TODO
+
+var toool = new Tool();
+
+var c = Shape.Circle(200, 200, 80);
+c.fillColor = 'black';
+var text = new PointText(200, 200);
+text.justification = 'center';
+text.fillColor = 'white';
+text.fontSize = 20;
+text.content = 'hello world';
+
+toool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'green';
+};
+
+paper.view.draw();
